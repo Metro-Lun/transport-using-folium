b0cc5f915fa2b036f0eaca53aa1d731392dbccfa
Metro-Lun <mehdi.bourbon@outlook.fr>
2022-04-14 19:31:23 +0200

    Removed useless files.

--- act4_bdd.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("requetes.sqlite3")
-c = connection.cursor()
-
-c.execute("SELECT * FROM Arrets")
-
-data_list = c.fetchall()	# obtain the results as a list
-print(f"1) I've got {len(data_list)} saves in my data base.")
